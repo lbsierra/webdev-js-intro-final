2e99789694ab365c28f70d6ccbe74b177140894f
Sierra <sonicaradia@gmail.com>
2025-01-19 14:34:52 -0500

    Wrote Javascript for number guessing game

--- README.md
+++ README.md
@@ -37,13 +37,13 @@ All elements that will be referenced in `app.js` will have an id. Read through `
 
 If you get stuck, write psuedocode and break the game's functionality and logic into smaller pieces.
 
-- [ ] The span with the id of `guess-message` should output whether the guess is too high, too low, if the player won, or if the player lost.
-- [ ] The span with the id of `current-guess` should output the guess that was just submitted.
-- [ ] The span with the id of `computer-guess` should output the guess that was randomly generated.
-- [ ] The span with the id of `guess-history` should output your guess history.
-- [ ] The "Submit Guess" button should be enabled at the beginning of the game, but when the game is over it should be disabled.
-- [ ] The "Restart" button should be disabled at the beginning of the game, but when the game is over it should be enabled.
-- [ ] When the "Restart" button is clicked, all values and text output should reset.
+- [ X ] The span with the id of `guess-message` should output whether the guess is too high, too low, if the player won, or if the player lost.
+- [ X ] The span with the id of `current-guess` should output the guess that was just submitted.
+- [ X ] The span with the id of `computer-guess` should output the guess that was randomly generated.
+- [ X ] The span with the id of `guess-history` should output your guess history.
+- [ X ] The "Submit Guess" button should be enabled at the beginning of the game, but when the game is over it should be disabled.
+- [ X ] The "Restart" button should be disabled at the beginning of the game, but when the game is over it should be enabled.
+- [ X ] When the "Restart" button is clicked, all values and text output should reset.
 
 
 ### Tips
--- js/app.js
+++ js/app.js
@@ -1,1 +1,89 @@
 "use strict";
+
+const resultEl = document.getElementById("guess-message");
+const currentGuessEl = document.getElementById("current-guess");
+const computerGuessEl = document.getElementById("computer-guess");
+const guessInputEl = document.getElementById("guess-input");
+const guessHistoryEl = document.getElementById("guess-history");
+const submissionBtn = document.getElementById("submit-btn")
+const restartBtn = document.getElementById("restart-btn");
+
+let currentGuess;
+let playerGuesses = [];
+let remainingGuesses = 3;
+let computerNumber;
+
+function updateComputerGuess() {
+    let min = 1; 
+    let max = 10; 
+    computerNumber = Math.floor((Math.random() * (max - min)) + min);
+    computerGuessEl.innerHTML = computerNumber;
+}
+
+function updateCurrentGuess() {
+    currentGuess = guessInputEl.value; 
+    currentGuessEl.innerHTML = currentGuess;  
+ }
+
+function updateResult() {
+    if (currentGuess > computerNumber) {
+    resultEl.innerHTML = "Too high!"
+} else if (currentGuess < computerNumber) {
+    resultEl.innerHTML = "Too low!"
+} else if (currentGuess === computerNumber) {
+    resultEl.innerHTML = "You win! You guessed correctly!";
+} 
+}
+
+function updatePlayerGuesses() {
+    playerGuesses.push(currentGuess);
+    if (remainingGuesses > 0) {
+    remainingGuesses -= 1;
+    }  
+   }
+
+function updateGuessHistory() {
+    guessHistoryEl.innerHTML = "";
+   let i;
+    for (i = 0; i < playerGuesses.length; i++) {
+       guessHistoryEl.innerHTML += playerGuesses[i] + " ";
+    }    
+}
+
+
+
+function gameOver() {
+    if (remainingGuesses === 0) {
+        submissionBtn.disabled = true;
+        restartBtn.disabled = false;
+        resultEl.innerHTML = "You lose. Try again!";
+    }
+}
+
+function render() {
+    updateComputerGuess();
+    updateCurrentGuess();
+    updateResult();  
+    updatePlayerGuesses();  
+    updateGuessHistory();
+    gameOver();
+}
+
+submissionBtn.addEventListener("click", function () {
+    render();
+});
+
+function resetGame() {
+    resultEl.innerHTML = "";
+    currentGuessEl.innerHTML = "";
+    guessHistoryEl.innerHTML = "";
+    computerGuessEl.innerHTML = "";
+    remainingGuesses = 3;
+    playerGuesses = [];
+    submissionBtn.disabled = false;
+    restartBtn.disabled = true;
+} 
+
+restartBtn.addEventListener("click", function () {
+    resetGame();
+});
